f5c219755255f75f027216b27a34d74acc0a8538
LINDEX171 <lindex171@gmail.com>
2025-01-06 12:27:48 +0000

    Table widget

---
+++ lib/table_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TablePage extends StatefulWidget {
+  const TablePage({super.key});
+
+  @override
+  State<TablePage> createState() => _TablePageState();
+}
+
+class _TablePageState extends State<TablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Table(
+            border: TableBorder.all(),
+        children: [
+          TableRow(children: [
+            Text('Colonne 1'),
+            Text('Colonne 2'),
+            Text('Colonne 3'),
+          ]),
+          TableRow(children: [
+            Text('Colonne 1'),
+            Text('Colonne 2'),
+            Text('Colonne 3'),
+          ])
+        ],
+      )),
+    );
+  }
+}
