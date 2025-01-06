47e752698b1a2a52bf762ed2106753a371776023
LINDEX171 <lindex171@gmail.com>
2025-01-06 11:09:33 +0000

    gesture detector

--- lib/stack_page.dart
+++ lib/stack_page.dart
@@ -8,39 +8,11 @@ class StackPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Image avec Badge')),
       body: Center(
-        child: Stack(
-          clipBehavior: Clip.none, // Permet de gérer les éléments qui dépassent
-          children: [
-            // Image de profil
-            CircleAvatar(
-              radius: 50, // Taille de l'image
-              backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.POYKy-CDp5DUJymdQ9myXQHaHj?rs=1&pid=ImgDetMain'), // Chemin de l'image
-            ),
-
-            // Badge
-            Positioned(
-              bottom: -5, // Position verticale
-              right: -5, // Position horizontale
-              child: Container(
-                width: 25,
-                height: 25,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.white, // Bordure blanche autour du badge
-                    width: 2,
-                  ),
-                ),
-                child: Icon(
-                  Icons.check, // Icône dans le badge
-                  color: Colors.white,
-                  size: 15,
-                ),
-              ),
-            ),
-          ],
-        ),
+       child: GestureDetector(onTap: () => print('Widget tapé !'), child: Container(
+         color: Colors.blue,
+         width: 100,
+         height: 100,
+       ),),
       ),
     );
   }
