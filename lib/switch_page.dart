8e87c94c88496971fc5793d4f05cfb121221fa3c
LINDEX171 <lindex171@gmail.com>
2025-01-08 11:00:14 +0000

    Checkbox true & false

--- lib/switch_page.dart
+++ lib/switch_page.dart
@@ -11,14 +11,37 @@ class _SwitchPageState extends State<SwitchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Checkbox(
-        value: true, // État actuel (vrai ou faux)
-        onChanged: (value) {
+      body:  CheckboxExample(),
+    );
+  }
+}
 
-        },
-    ),
-      )
+
+class CheckboxExample extends StatefulWidget {
+  @override
+  _CheckboxExampleState createState() => _CheckboxExampleState();
+}
+
+class _CheckboxExampleState extends State<CheckboxExample> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Checkbox(
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            },
+          ),
+          Text(isChecked ? "Activé" : "Désactivé"),
+        ],
+      ),
     );
   }
 }
