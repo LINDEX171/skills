13cc2f46b3e6484f62c7c65807b99c8e370ed452
LINDEX171 <lindex171@gmail.com>
2025-01-09 11:08:28 +0000

    advanced card widget

--- lib/card_page.dart
+++ lib/card_page.dart
@@ -8,20 +8,45 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          child: Card(
-            color: Colors.deepOrange,
-            elevation: 4,
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Text("Je suis une carte"),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: <Widget>[
+          Card(
+            elevation: 5, // Ombre de la carte
+            shape: RoundedRectangleBorder( // Forme arrondie
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              contentPadding: EdgeInsets.all(16),
+              leading: Icon(Icons.info),
+              title: Text('Titre de la Carte'),
+              subtitle: Text('Sous-titre de la carte'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Action lorsqu'on appuie sur la carte
+                print('Card tapped!');
+              },
             ),
           ),
-        ),
+          SizedBox(height: 20),
+          Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              contentPadding: EdgeInsets.all(16),
+              leading: Icon(Icons.star),
+              title: Text('Autre Carte'),
+              subtitle: Text('Détails supplémentaires'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Autre card tapped!');
+              },
+            ),
+          ),
+        ],
       ),
-    );
+      );
   }
 }
