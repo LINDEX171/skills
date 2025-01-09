48963962cd2f767a6a47c0c7a2a144bdbe5655da
LINDEX171 <lindex171@gmail.com>
2025-01-09 11:17:31 +0000

    add image on card

--- lib/card_page.dart
+++ lib/card_page.dart
@@ -8,45 +8,35 @@ class CardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: <Widget>[
-          Card(
-            elevation: 5, // Ombre de la carte
-            shape: RoundedRectangleBorder( // Forme arrondie
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16),
-              leading: Icon(Icons.info),
-              title: Text('Titre de la Carte'),
-              subtitle: Text('Sous-titre de la carte'),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                // Action lorsqu'on appuie sur la carte
-                print('Card tapped!');
-              },
-            ),
+      body: Container(
+        height: 400,
+        width: 200,
+        child: Card(
+          color: Colors.orange,
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-          SizedBox(height: 20),
-          Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16),
-              leading: Icon(Icons.star),
-              title: Text('Autre Carte'),
-              subtitle: Text('Détails supplémentaires'),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                print('Autre card tapped!');
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network('https://th.bing.com/th/id/R.b46d1ed422dccbfe1310814fcbe77688?rik=RfqrC3rKCP1%2fWw&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fcards-hd-png-playing-cards-png-2030.png&ehk=sXTpg%2bLeUkb3D0JZMtkVtOszpJ%2f%2b7EqpQt8N2oPMmOk%3d&risl=1&pid=ImgRaw&r=0'), // Image dans la carte
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Titre avec Image',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text('Sous-titre ou description de la carte'),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      )
+      ,
       );
   }
 }
